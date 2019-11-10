3d7c77c7b831f3b2836bcc3d9b9af5a65d949dd3
FelipeMFlores <felipe.melloflores@gmail.com>
2019-11-10 10:15:38 -0300

    parameters function

--- detectAxis.m
+++ detectAxis.m
@@ -1,20 +1,26 @@
-function detectAxis()
-I = imread('exemplo1.jpg');
+function detectAxis(image, numPeaks, rotatePeak, nLines)
+%image = 'examplo1.jpg' , numpeaks = 2 , rotatePeak = 2 , nLines = 3
+I = imread(image);
 I2 = rgb2gray(I);
 BW = edge(I2,'canny', 0.45);
 
 %find the rotation to align with a axis
-[H,theta,rho] = hough(BW);
-P = houghpeaks(H,2,'threshold',ceil(0.3*max(H(:))));
+[H,theta,~] = hough(BW);
+P = houghpeaks(H ,numPeaks);
 
-thetaPeak = theta(P(2,2));
+thetaPeak = theta(P(rotatePeak, 2));
 % Fix the image
 BW = imrotate(BW,thetaPeak);
 %calculate new lines
 [H,theta,rho] = hough(BW);
-P = houghpeaks(H,2,'threshold',ceil(0.3*max(H(:))));
+P = houghpeaks(H,numPeaks,'threshold',ceil(0.3*max(H(:))));
 lines = houghlines(BW,theta,rho,P,'FillGap',300,'MinLength',10);
 
+imshow(imrotate(I, thetaPeak)), hold on
+for k = 1:length(lines)
+   xy = [lines(k).point1; lines(k).point2];
+   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','c');
+end
 %find intersection of x and y axis
 int = linlinintersect([lines(1).point1;lines(1).point2;lines(2).point1;lines(2).point2]);
 xAxis1 = [lines(2).point1(1) , lines(2).point2(1)];
@@ -24,18 +30,8 @@ int2 = [int(1)+500,int(2)];
 yAxis1 = [int(1)-200 , int2(1)];
 yAxis2 = [int(2) , int2(2)];    
 
-imshow(BW), hold on;
-for k = 1:length(lines)
-   x = [lines(k).point1(1); lines(k).point2(1)];
-   y = [lines(k).point1(2); lines(k).point2(2)];
-   plot(x,y,'LineWidth',7,'Color','black');
-end
-
-clf;
-
 % get all white points
 [y, x] = find(BW == 1);
-imshow(imrotate(I, thetaPeak)), hold on
 %plot(x, y, '.')
 
 % draw axis
@@ -44,35 +40,29 @@ plot(yAxis1,yAxis2,'LineWidth',2,'Color','yellow');
 
 
 % Use mls with ransac two times, to find and discard the two axis
-
 N = 1;           % first-degree polynomial
 maxDistance = 20; % maximum allowed distance for a point to be inlier
+for i = 1:nLines+1
+    [P, inlierIdx] = fitPolynomialRANSAC([y, x],N,maxDistance);
+    yRecoveredCurve = polyval(P,x);
+    if i == 1
+        %plot(yRecoveredCurve, x,'-g','LineWidth',2)
+        %plot(x(inlierIdx),y(inlierIdx),'.', 'Color', 'blue')
+        %plot(x(~inlierIdx),y(~inlierIdx),'ro','Color','red')
+    end
 
-[P, inlierIdx] = fitPolynomialRANSAC([y, x],N,maxDistance);
-yRecoveredCurve = polyval(P,x);
-%plot(yRecoveredCurve, x,'-g','LineWidth',2)
-%plot(x(inlierIdx),y(inlierIdx),'.', 'Color', 'blue')
-%plot(x(~inlierIdx),y(~inlierIdx),'ro','Color','red')
+    %discard
+    x = x(~inlierIdx);
+    y = y(~inlierIdx);
+end
 
-%discard
-x = x(~inlierIdx);
-y = y(~inlierIdx);
-
-[P, inlierIdx] = fitPolynomialRANSAC([y, x],N,maxDistance);
-yRecoveredCurve = polyval(P,x);
-%plot(yRecoveredCurve, x,'-g','LineWidth',2)
-%plot(x(inlierIdx),y(inlierIdx),'.', 'Color', 'blue')
-%plot(x(~inlierIdx),y(~inlierIdx),'ro','Color','red')
-
-% discard
-x = x(~inlierIdx);
-y = y(~inlierIdx);
 
 % use mls and ransac to find the seconde degree polynomial (parable)
 N=2;
+maxDistance = 25;
 [P, inlierIdx] = fitPolynomialRANSAC([y, x],N,maxDistance);
 yRecoveredCurve = polyval(P,x);
-plot(yRecoveredCurve, x,'-g','LineWidth',2)
+plot(yRecoveredCurve, x,'-b','LineWidth',2)
 %plot(x(inlierIdx),y(inlierIdx),'.', 'Color', 'blue')
 %plot(x(~inlierIdx),y(~inlierIdx),'ro','Color','red')
 
